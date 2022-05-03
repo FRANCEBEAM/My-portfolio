98520768a943041b087868e1a310809fbae0fdc9
FRANCEBEAM <santanderfrancisbeam@gmail.com>
2022-05-03 22:22:17 +0800

    first commit

---
+++ script/index.js
@@ -0,0 +1,35 @@
+const selection = document.querySelector('.selection');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menuItems = document.querySelectorAll('nav .selection li a');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menuItems.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    selection.style.display = 'flex';
+    selection.style.top = '0';
+}
+function close(){
+    selection.style.top = '-100%';
+}
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    centeredSlide: true,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
